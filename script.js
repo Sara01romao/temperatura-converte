5635e229ea31f1be1f70d42d9e2f5bd406b34a4f
Sara Romao <sara-romao@live.com>
2021-03-11 23:07:51 -0300

    configurando onchange no input

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 
+function teste(){
+      let valor= document.getElementById('v');
+
+   
+      if(valor !== " "){
+            document.getElementById("op").disabled=false;
+          
+            
+       }else{
+             document.getElementById("op").disabled = true;
+       }
+}
+
 function update(){
      
       var select= document.getElementById('op');
@@ -6,28 +19,38 @@ function update(){
       var c= "";
       var f= "";
 
+      
      
       // document.getElementById('aqui').innerHTML= valor;
+      
+     
+            if(valor=='cf'){
+                  c = document.getElementById('v').value;
+                  f= (c*1.8000+32);
+                  document.getElementById('aqui').innerHTML= f;
+                         
+            }else{
+                  f = document.getElementById('v').value;
+                  c=((f-32)/1.8000).toFixed();
+                  
+                  document.getElementById('aqui').innerHTML= c;
+                  
+                  
+            }
+      
+     
 
 
-      if(valor=='cf'){
-            c = document.getElementById('v').value;
-            f= c*1.8000+32;
-            document.getElementById('aqui').innerHTML= f;
-                   
-      }else{
-            f = document.getElementById('v').value;
-            c=(f-32)/1.8000;
-            document.getElementById('aqui').innerHTML= c;
-            
-            
+     
+      
+}
+
+
+ function clean(){
+            document.getElementById('v').value="";
+            document.getElementById('aqui').innerHTML= " ";
+            document.getElementById("op").value=" ";
+            document.getElementById("op").disabled=true;
+          
+       
       }
-
-}
-
-function clean(){
-      document.getElementById('v').value= " ";
-      document.getElementById('aqui').value= " ";
-    
- 
-}
